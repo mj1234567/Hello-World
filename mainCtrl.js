ef2c862712782f32f87fdbbc060423b715eedea1
Mj <mjpannu1@gmail.com>
2017-08-03 20:30:57 +0530

    Update mainCtrl.js

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,3 +1,43 @@
 angular.module('mainCtrl', [])
 
-.controller('MainController', function() {
+.controller('MainController', function($rootScope, $location, Auth) {
+      var vm = this;
+  
+      vm.loggedIn = Auth.isLogged();
+  
+      $rootScope.$on('$routeChangeStart', function() {
+          vm.loggedIn = Auth.isLogged();
+
+          Auth.getUser()
+                .then(function(date) {
+                        vm.user = data.data;
+                      }
+      })
+        
+      vm.doLogin = function() {
+        vm.processing = true;
+        vm.error = '';
+        
+        Auth.login(vm.loginData.username, vm.loginData.password)
+            .success(function(data) {
+                vm.processing = false;
+          
+                Auth.getUser()
+                    .then(function(data) {
+                        vm.user = data.data;
+                    });
+          
+                if(data.success) {
+                  $location.path('/');
+                else
+                  vm.error = data.message;
+                }
+            })
+      }
+      
+      vm.doLogout = function() {
+        Auth.logout();
+        $location.path('/logout');
+      }
+  
+});
